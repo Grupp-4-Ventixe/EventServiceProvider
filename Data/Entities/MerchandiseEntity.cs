9789515048bd84adaf46402b845f464f75f68fc2
Kimberly Hadjal <kimberlysseremba@outlook.com>
2025-05-09 10:28:17 +0200

    Added new entities and updated DbContext

---
+++ Data/Entities/MerchandiseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entities;
+
+public class MerchandiseEntity
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [ForeignKey("Event")]
+    public Guid EventId { get; set; }
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+    public string? ImageUrl { get; set; }
+    public EventEntity Event { get; set; } = null!;
+}
---
+++ Data/Entities/SeatPlanEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities;
+
+public class SeatPlanEntity
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [ForeignKey("Event")]
+    public Guid EventId { get; set; }
+
+    public EventEntity Event { get; set; } = null!;
+    public ICollection<SeatCategoryEntity> Categories { get; set; } = [];
+}
